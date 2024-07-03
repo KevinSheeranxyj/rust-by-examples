ef0fd4f50cbd84b63576dd14c2062fd5b315c943
Kevin Xie <kevin.xie@dtcpay.com>
2024-07-03 18:48:12 +0800

    UPDATE: struct related

--- structure_related/src/main.rs
+++ structure_related/src/main.rs
@@ -29,5 +29,17 @@ struct Unit;
 struct Pair(i32, i32);
 
 fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 
+    println!("six: {:?}", six);
+    println!("None: {:?}", none);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
